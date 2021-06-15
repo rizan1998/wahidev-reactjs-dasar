94b6a5e7975bada38de553aaa40e39f3e14ccf77
rizan1998 <rizanalfalah@gmail.com>
2021-06-15 13:30:33 +0700

    selesai mengikuti tutor wahidev tentang map, filter dan reduce sederhana

---
+++ src/Map.js
@@ -0,0 +1,59 @@
+//rfce
+import React from "react";
+
+const makanans = [
+  {
+    nama: "soto",
+    harga: 12000,
+  },
+  {
+    nama: "bakso",
+    harga: 10000,
+  },
+  {
+    nama: "mie ayam",
+    harga: 8000,
+  },
+  {
+    nama: "seblak",
+    harga: 5000,
+  },
+];
+
+// Reduce
+const totalBayar = makanans.reduce((totalHarga, makanan) => {
+  return totalHarga + makanan.harga;
+}, 0);
+
+function Map() {
+  return (
+    <div>
+      {/* Map */}
+      <h2>Map Sederhana</h2>
+      <ul>
+        {makanans.map((makanan, index) => (
+          <li>
+            {index + 1}. {makanan.nama} - {makanan.harga}
+          </li>
+        ))}
+      </ul>
+      <hr></hr>
+
+      {/* filter */}
+      <h2>Map Filter Harga yang lebih dari 11.000</h2>
+      <ul>
+        {makanans
+          .filter((makanan) => makanan.harga > 11000)
+          .map((makanan, index) => (
+            <li>
+              {index + 1}. {makanan.nama} - {makanan.harga}
+            </li>
+          ))}
+      </ul>
+
+      <h3>Total Harga : {totalBayar}</h3>
+    </div>
+  );
+}
+
+export default Map;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
+import Map from "./Map";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Map />
   </React.StrictMode>,
   document.getElementById("root")
 );
