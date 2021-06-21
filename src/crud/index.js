3d3a9569cfddfc144d3aec5d5330c20998393e4e
rizan1998 <rizanalfalah@gmail.com>
2021-06-21 10:55:57 +0700

    component
    mencoba membuat component

--- src/crud/index.js
+++ src/crud/index.js
@@ -1,7 +1,34 @@
 import React, { Component } from "react";
+import FormulirComponents from "./FormulirComponents";
 import NavbarComponent from "./NavbarComponent";
+import TableComponents from "./TableComponents";
 
 export default class index extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      makanans: [],
+      nama: "",
+      deskripsi: "",
+      harga: 0,
+      id: "",
+    };
+  }
+
+  //handle change
+  handleChange = (event) => {
+    this.setState({
+      //maksud target name disini adalah nama propery state dan valuenya
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  //handle submit
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   render() {
     return (
       <div>
@@ -10,6 +37,8 @@ export default class index extends Component {
           <div class="mt-4">
             <h2>Hellow World!</h2>
           </div>
+          <TableComponents />
+          <FormulirComponents {...this.state} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
         </div>
       </div>
     );
