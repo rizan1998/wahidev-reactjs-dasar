d0a332d64c6118e833e1639b076bccfdaf7dec4d
rizan1998 <rizanalfalah@gmail.com>
2021-06-22 11:27:26 +0700

    Edit button component
    membuat funciton dan memberi saran dengan komentar bahwa button perlu props warna

---
+++ src/micro_component/Button_component.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//tambah props untuk warna
+function Button_component() {
+  return (
+    <div>
+      <button></button>
+    </div>
+  );
+}
+
+export default Button_component;
